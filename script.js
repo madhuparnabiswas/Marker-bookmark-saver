c598b02c84494e68d8940bb1f6fa77e8a56d0194
Madhuparna Biswas <madhuparnabiswas06@gmail.com>
2026-03-30 23:10:30 +0530

    updated

---
+++ script.js
@@ -0,0 +1,13 @@
+const usernameInput = document.querySelector("#username-box");
+const errorMessage = document.querySelector("#username-error");
+
+usernameInput.addEventListener("input", () => {
+  if (usernameInput.value.trim().length < 3 && usernameInput.value.trim().length > 0) {
+    errorMessage.textContent = "Minimum 3 characters required";
+  } else {
+    errorMessage.textContent = "";
+  }
+});
+
+console.log(usernameInput);
+console.log(errorMessage);
